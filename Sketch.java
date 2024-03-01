9344a52008cc765fd3b50cf968017084aafc366b
Caeleny <caelen.yeung25@ycdsbk12.ca>
2024-03-01 12:43:44 -0500

    submit

--- Sketch.java
+++ Sketch.java
@@ -2,35 +2,47 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+
   public void settings() {
-	// put your size call here
+	// Background size
     size(400, 400);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
+  // Background color
   public void setup() {
-    background(210, 255, 173);
+    background(67, 170, 239);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
+  // Different shapes
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+	// House
+    fill(255, 220, 150);
+    rect(40, 200, 300, 200);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
+    // Door
+    fill(66, 69, 62);
+    rect(300, 330, 40, 70);
+
+    // Window
+    fill(120, 205, 255);
+    rect(210, 230, 100, 70);
+
+    // Window 2
+    fill(120, 205, 255);
+    rect(70, 230, 100, 70);
+
+    // Window 3 
+    fill(120, 205, 255);
+    rect(70, 320, 150, 70);
+
+    // Roof
+    fill(163, 112, 49);
+    triangle(30, 200, 190, 100, 350, 200);
   
-  // define other methods down here.
+    // Sun
+    fill(255, 242, 0);
+    ellipse(50,50, 100, 100); 
+  }
+
 }
